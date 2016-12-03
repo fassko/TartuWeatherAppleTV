4d05e63a711a62811cc62f66c60577e065ed42f2
Kristaps Grinbergs <kristaps@qminderapp.com>
2016-12-03 12:26:47 +0200

    Initial Commit

---
+++ TartuWeatherTV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TartuWeatherTV
+//
+//  Created by Kristaps Grinbergs on 03/12/2016.
+//  Copyright © 2016 Kristaps Grinbergs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
